12175ff3a42c41f4b21deaba27c77e6ca958bcf7
deivid123-skcet <deividkelven1@hotmail.com>
2022-04-12 19:03:53 -0300

    Atualizaçao

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,24 +2,44 @@ print("*********************************")
 print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
+import random
 
-chute_str = input("Digite o seu Numero : ")
+numero_secreto = int(random.randrange(1,101)) 
+total_de_tentativas = 0
+rodada = 1
+print("Qual nivel de dificuldade?")
+print("(1) Fácil (2) Médio (3) Díficil")
 
-print("Você digitou:", chute_str)
+nivel =  int(input("Defina um nível: "))
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else: 
+    total_de_tentativas = 5
 
-chute = int(chute_str)
 
-acertou = (chute == numero_secreto)
-maior = (chute > numero_secreto)
-menor = (chute < numero_secreto)
+for rodada in range(1,total_de_tentativas + 1): 
+    print("Tentativas {:02d} de {:02d}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite um número entre 1 e 100 : ")
+    print("Você digitou:", chute_str)
+    chute = int(chute_str)
 
-if acertou:
-    print("Parabens voce acertou!")
-else:
-    if maior:
-        print("voce errou! O seu chute foi maior do que o número secreto")
-    elif menor:
-        print("voce errou! O seu chute foi menor do que o número secreto")
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100")
+        continue
+
+    acertou = (chute == numero_secreto)
+    maior = (chute > numero_secreto)
+    menor = (chute < numero_secreto)
+
+    if (acertou):
+        print("Parabens voce acertou!")
+        break
+    else:
+        if (maior):
+            print("voce errou! O seu chute foi maior do que o número secreto")
+        elif (menor):
+            print("voce errou! O seu chute foi menor do que o número secreto")
 
 print("Fim do Jogo!")
