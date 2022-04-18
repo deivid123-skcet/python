f8bca3b55c8e6c4a7db90613bad4207c2003997e
deivid123-skcet <deividkelven1@hotmail.com>
2022-04-18 11:05:59 -0300

    desenvolvimento forca

--- forca.py
+++ forca.py
@@ -1,7 +1,29 @@
+from operator import index
+
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
+
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        
+        chute = input("Qual Letra?")
+        chute = chute.strip()
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei  a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("jogando...")
+
     print("Fim de Jogo")
+
 if(__name__ == "__main__"):
     jogar()
+
